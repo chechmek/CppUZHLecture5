08757a6dcd6cb6487002929d9875983c87ca3a26
Roman Babukh <romanklut89@gmail.com>
2022-11-23 13:15:06 +0100

    Init

---
+++ ReimplementMerge/ReimplementMerge.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+#include <list>
+
+using namespace std;
+
+template<typename T1, typename T2>
+void merge(T1& c1, const T2& c2) {
+	copy(c2.begin(), c2.end(), back_inserter(c1));
+}
+
+int main()
+{
+	vector<int> v1 = { 1, 2 ,3 };
+	vector<int> v2 = { 4, 5, 6 };
+	merge(v1, v2);
+	
+	list<int> l1 = { 5, 6, 7 };
+	merge(l1, v2);
+
+	cout << "merged vector" << endl;
+	for (auto it : v1) {
+		cout << it << endl;
+	}
+	cout << "merged list and vector" << endl;
+	for (auto it : l1) {
+		cout << it << endl;
+	}
+}
---
+++ SquaresAndRectangles/SquaresAndRectangles.cpp
@@ -0,0 +1,79 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct Shape {
+    virtual float square();
+};
+
+struct Triangle : public Shape {
+    float a, b, c;
+    float square() override{
+        float s = (float)(a + b + c) / 2;
+        return sqrt(s * (s - a) * (s - b) * (s - c));
+    }
+};
+struct Circle : public Shape {
+    float r;
+    float square() override {
+        return 3.14 * r * r;
+    }
+};
+struct Line : public Shape {
+    float length;
+    float square() override {
+        return 0;
+    }
+};
+struct Rectangle : public Shape {
+    float a, b;
+    float square() override {
+        return a*b;
+    }
+    void set_sides(float a1, float b1) {
+        a = a1;
+        b = b1;
+    }
+};
+struct Square : public Rectangle {
+    float square() override {
+        return a * a;
+    }
+    void set_sides(float a1, float b1) {
+        // what to do?
+    }
+};
+
+//struct Square : public Shape {
+//    float a;
+//    float square() override {
+//        return a * a;
+//    }
+//    void set_side(float a1) {
+//        a = a1;
+//    }
+//};
+//struct Rectangle : public Square {
+//    float b;
+//    float square() override {
+//        return a * b;
+//    }
+//    void set_sides(float a1, float b1) {
+//        a = a1;
+//        b = b1;
+//    }
+//};
+//struct Triangle : public Rectangle {
+//    float c;
+//    float square() override {
+//        float s = (float)(a + b + c) / 2;
+//        return sqrt(s * (s - a) * (s - b) * (s - c));
+//    }
+//};
+
+int main()
+{
+    vector<Rectangle> shapes { Rectangle(), Rectangle(), Rectangle(), Square() };
+
+}
